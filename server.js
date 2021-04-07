4294bab790075365c8eb0660c952cb9252e6df45
rishikasingh7055 <rishika.singh_cs18@gla.ac.in>
2021-04-07 12:06:27 +0530

    addition

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,12 +12,15 @@ app.use(express.json());
 
 const connectDB = require('./config/db'); 
 connectDB();
-
+const corsOption ={
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOption));
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use('/api/files',require('./routes/files'));
-app.use('/files', require('./routes/show'));
+app.use('/files', require('./routes/show')); 
 app.use('/files/download', require('./routes/download'));
 
 app.listen(PORT, () => {
